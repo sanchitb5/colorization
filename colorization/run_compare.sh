3165906cb812cfd8c4f524d69154fff3653107eb
Ubuntu <ubuntu@ip-172-31-91-1.ec2.internal>
2017-12-11 07:41:48 +0000

    Add script

---
+++ colorization/run_compare.sh
@@ -0,0 +1,11 @@
+picsdir = $0
+crop="${picsdir}crop.jpg"
+bw="${picsdir}bw.png"
+tess="${picsdir}tess.png"
+bwres = "${picsdir}bwres.png"
+tessres = "${picsdir}tessres.png"
+tessrescrop = "${picsdir}tessrescrop.png"
+python ../flip_tess.py -img_in $crop -img_out $0
+python colorization/colorize.py -img_in $bw -img_out $bwres
+python colorization/colorize.py -img_in $tess -img_out $tessres
+python ../crop_tess.py -img_in $tessres -img_out $tessrescrop
